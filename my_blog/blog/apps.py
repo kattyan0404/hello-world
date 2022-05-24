790171ea0270c70729a29493578130bfa6576ad6
kk-laptop <kattyan04042016@outlook.jp>
2022-05-24 09:29:53 +0900

    AP create

--- my_blog/blog/apps.py
+++ my_blog/blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppBlogConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_blog'
+    name = 'blog'
