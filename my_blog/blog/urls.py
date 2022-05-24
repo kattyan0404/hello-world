3ea32cce71b8f0e411b0feddb2ebbb455835d18b
kk-laptop <kattyan04042016@outlook.jp>
2022-05-24 11:22:49 +0900

    READ-CREATE

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'app_blog'
 
 urlpatterns = [
-    path('/index', views.IndexView.as_view(), name='index')
+    path('index', views.IndexView.as_view(), name='index')
 ]
