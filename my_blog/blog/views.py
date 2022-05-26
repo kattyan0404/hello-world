4ad9a47dd08893a68f5251aef8a32a4591a451be
kk-laptop <kattyan04042016@outlook.jp>
2022-05-26 10:39:55 +0900

    edit-html-view

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -3,3 +3,8 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "blog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["test_text"] = "This is Django test text!"
+        return context
